a315bc6127dccb94608394a798c0f6533949d78f
amjayem <csejayem19@gmail.com>
2024-03-26 00:14:45 +0600

    product table added with functionalities

---
+++ src/app/product/columns.tsx
@@ -0,0 +1,124 @@
+'use client'
+
+import { ColumnDef } from '@tanstack/react-table'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu'
+import { ArrowUpDown, MoreHorizontal } from 'lucide-react'
+import { Checkbox } from '@/components/ui/checkbox'
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+
+export type Product = {
+  _id: string
+  productTitle: string
+  productWholesalePrice: string
+  productRetailPrice: string
+  productQuantity: string
+}
+
+export const columns: ColumnDef<Product>[] = [
+  {
+    id: 'select',
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && 'indeterminate')
+        }
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label='Select all'
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label='Select row'
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false
+  },
+  {
+    accessorKey: 'productTitle',
+    header: 'Product Title',
+    cell: ({ row }) => (
+      <div className='capitalize'>{row.getValue('productTitle')}</div>
+    )
+  },
+  {
+    accessorKey: 'productWholesalePrice',
+    header: ({ column }) => {
+      const colData = Number(column)
+      return (
+        <Button
+          variant='ghost'
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}>
+          Wholesale Price
+          <ArrowUpDown className='ml-2 h-4 w-4' />
+        </Button>
+      )
+    },
+    cell: ({ row }) => (
+      <div className='lowercase'>{row.getValue('productWholesalePrice')}</div>
+    )
+  },
+  {
+    accessorKey: 'productRetailPrice',
+    header: () => <div className='text-right'>Retail Price</div>,
+    cell: ({ row }) => {
+      const amount = parseFloat(row.getValue('productRetailPrice'))
+
+      // Format the amount as a dollar amount
+      const formatted = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'bdt'
+      }).format(amount)
+
+      return <div className='text-right font-medium'>{formatted}</div>
+    }
+  },
+  {
+    id: 'actions',
+    enableHiding: false,
+    cell: ({ row }) => {
+      const product = row.original
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant='ghost' className='h-8 w-8 p-0'>
+              <span className='sr-only'>Open menu</span>
+              <MoreHorizontal className='h-4 w-4' />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align='end'>
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() => {
+                const productData = `
+                  ${product?.productTitle},
+                  Whole salePrice: ${product?.productWholesalePrice},
+                  Retail Price: ${product?.productRetailPrice},
+                  product Quantity: ${product?.productQuantity}
+                `
+                navigator.clipboard.writeText(productData)
+              }}>
+              Copy product data
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <Link href={`/product/${product?._id}`}>Edit</Link>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      )
+    }
+  }
+]
--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,58 +1,23 @@
+import { DataTable } from '@/components/data-table'
 import { Button } from '@/components/ui/button'
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle
-} from '@/components/ui/card'
 import { getAllProducts } from '@/lib/apis/product'
-import { ColumnDef } from '@tanstack/react-table'
 import Link from 'next/link'
 import React from 'react'
+import { columns } from './columns'
+
+export type Product = {
+  id: string
+  productTitle: string
+  productWholesalePrice: string
+  productRetailPrice: string
+}
 
 // Define component function
 const AddProduct: React.FC = async () => {
   const response = await getAllProducts()
   const totalProducts = response.data.totalCount
   const products = response.data.data
-  console.log(products)
-
-  type Product = {
-    _id: string
-    productTitle: string
-    productQuantity: string
-    productWholesalePrice: string
-    productRetailPrice: string
-    note: string
-    createdAt: string
-    updatedAt: string
-    __v: number
-  }
-  const columns: ColumnDef<Product>[] = [
-    {
-      accessorKey: 'productTitle',
-      header: 'Product Title',
-      cell: ({ row }) => <div>{row.getValue('productTitle')}</div>
-    },
-    {
-      accessorKey: 'productQuantity',
-      header: 'Product Quantity',
-      cell: ({ row }) => <div>{row.getValue('productQuantity')}</div>
-    },
-    {
-      accessorKey: 'productWholesalePrice',
-      header: 'Product Wholesale Price',
-      cell: ({ row }) => <div>{row.getValue('productWholesalePrice')}</div>
-    },
-    {
-      accessorKey: 'productRetailPrice',
-      header: 'Product Retail Price',
-      cell: ({ row }) => <div>{row.getValue('productRetailPrice')}</div>
-    }
-    // Add more columns as needed...
-  ]
+  // console.log(products)
 
   return (
     <div>
@@ -60,26 +25,7 @@ const AddProduct: React.FC = async () => {
       <Button className='my-5'>
         <Link href={'/product/new-product'}>Add new product</Link>
       </Button>
-      <div className='flex gap-5'>
-        {products.map((product: any) => (
-          <Card className='w-full'>
-            <CardHeader>
-              <CardTitle>{product.productTitle}</CardTitle>
-              <CardDescription>{product.note}</CardDescription>
-            </CardHeader>
-            <CardContent>
-              <p>{product.productQuantity}</p>
-              <p>{product.productWholesalePrice}</p>
-              <p>{product.productRetailPrice}</p>
-            </CardContent>
-            <CardFooter>
-              <Button>
-                <Link href={`product/${product._id}`}>Edit</Link>
-              </Button>
-            </CardFooter>
-          </Card>
-        ))}
-      </div>
+      <DataTable columns={columns} data={products} />
     </div>
   )
 }
