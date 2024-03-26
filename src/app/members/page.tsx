4561852003da3e72008ed197f1a6fb4860116383
amjayem <csejayem19@gmail.com>
2024-03-27 00:23:45 +0600

    data fetching method updated, fixed error

--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -1,46 +1,27 @@
-'use client'
-import axiosInstance from '@/hooks/axiosInstance'
+import { Button } from '@/components/ui/button'
+import { getAllMembers } from '@/lib/apis/member'
 import Link from 'next/link'
-import React, { useEffect, useState } from 'react'
-import { FaArrowLeft } from 'react-icons/fa6'
 import { columns } from './columns'
 import { DataTable } from './data-table'
-import { Button } from '@/components/ui/button'
 
-interface Member {
-  fullName: string
-  _id: string
-  email: string
-  phoneNumber: string
-  address1: string
-  fundSource: string
-  image: string
-  gender: string
-}
+// interface Member {
+//   fullName: string
+//   _id: string
+//   email: string
+//   phoneNumber: string
+//   address1: string
+//   fundSource: string
+//   image: string
+//   gender: string
+// }
 
-type ComboBoxProps = {
-  open: boolean
-  setOpen: React.Dispatch<React.SetStateAction<boolean>>
-  value: string // replace with the actual type of your 'value' state
-  setValue: React.Dispatch<React.SetStateAction<string>> // replace with the actual type of your 'value' state
-}
-
-const AllMembers = () => {
-  const [memberList, setMemberList] = useState<Member[]>([])
-  useEffect(() => {
-    try {
-      axiosInstance.get('/all-members').then((response) => {
-        console.log(response.data)
-        setMemberList(response?.data?.allUsers)
-      })
-    } catch (error) {
-      console.log(error)
-    }
-  }, [])
+const AllMembers = async () => {
+  const response = await getAllMembers()
+  const data = response?.data?.allUsers
 
   return (
     <div className='flex flex-row gap-5 flex-wrap '>
-      {memberList.length == 0 && (
+      {data.length == 0 && (
         <text className='text-red-500'>
           No user found. Pleas add a Member first.
         </text>
@@ -51,7 +32,7 @@ const AllMembers = () => {
           <Link href={'/members/new'}>Add new member</Link>
         </Button>
       </div>
-      <DataTable columns={columns} data={memberList} />
+      <DataTable columns={columns} data={data} />
     </div>
   )
 }
