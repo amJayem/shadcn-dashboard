fe22076727f270bf324070abf8a5d781b7ab5b02
amjayem <csejayem19@gmail.com>
2024-05-11 23:48:17 +0600

    text modified

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import { Toaster } from '@/components/ui/toaster'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'ROF-Invertors',
+  title: 'ROF-Investors',
   description: 'Rajbari Organic Foods'
 }
 
